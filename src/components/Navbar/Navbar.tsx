a95c04a74ed37df61d9f00adc4cabb15ded5e234
Srutarshi Ghosh <srutarshighosh06@gmail.com>
2024-05-02 09:13:54 +0530

    save

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+	const loggedInNav = () => {};
+
+	return (
+		<div className={styles.navbar}>
+			<div className={styles["nav-container"]}>
+				<div className={styles["left-nav-section"]}>
+					<div className={styles.navLogo}>
+						<Link
+							to="/"
+							aria-label="HomePage"
+						>
+							<img
+								className={styles.imgFluid}
+								src="/_nuxt/Logo2.679479c7.svg"
+								alt="IndiaP2P Website Logo"
+							/>
+						</Link>
+					</div>
+				</div>
+				<div className={styles["right-nav-section"]}>
+					<div className={styles.navbar}>
+						<ul className={styles["nav-list"]}>
+							<li className={styles["nav-item"]}>
+								<a
+									href="/signup"
+									className={styles["nav-link"]}
+								>
+									Signup
+								</a>
+							</li>
+							<li className={styles["nav-item"]}>
+								<a
+									href="/login"
+									className={styles["nav-link"]}
+								>
+									Login
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
---
+++ src/layouts/HomePage/HomePage.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import landingImg from "../../assets/landing-page-image.png";
+import Navbar from "../../components/Navbar/Navbar";
+
+const Homepage = () => {
+	return (
+		<>
+			<Navbar />
+			<div className="container text-center">
+				<div className="row">
+					<h1>Welcome to LendMo!</h1>
+				</div>
+				<div className="row mt-5 align-items-center">
+					<div className="col-md-6">
+						<p className="">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero consequuntur voluptatibus, quod magni ducimus repellendus fuga, sunt adipisci corrupti dolores quis!
+							Ipsa impedit voluptates cum incidunt reiciendis consequuntur earum placeat! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis aperiam nobis quaerat
+							ad inventore facere quia distinctio perspiciatis incidunt! Amet ipsum totam repellat quod magni mollitia perferendis, unde ab modi!
+						</p>
+						<Link
+							className="btn btn-dark btn-lg  mt-4"
+							to="/signup"
+						>
+							Get Started
+						</Link>
+					</div>
+					<div className="col-md-6">
+						<img
+							src={landingImg}
+							width={400}
+							className="img-fluid mt-md-0 mt-5"
+							alt="Landing"
+						/>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Homepage;
