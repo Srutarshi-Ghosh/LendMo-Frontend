de564052c491d2fdf6536a05f1feda3060059829
Srutarshi Ghosh <srutarshighosh06@gmail.com>
2024-06-03 14:57:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import Homepage from './layouts/HomePage/HomePage';
+import Homepage from './layouts/HomePage';
 
 function App() {
   return (
     <div className="App">
+			<Navbar />
       <Homepage />
     </div>
   );
