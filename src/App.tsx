d96b47127df6e6e72c35cfc18de21b6c288d8fbd
Srutarshi Ghosh <srutarshighosh06@gmail.com>
2024-06-09 09:57:07 +0530

    Added custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,27 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./layouts/HomePage";
 import Layout from "./layouts/Layout";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./Theme";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<div className="App">
-				<Layout>
-					<Routes>
-						<Route
-							path="/"
-							element={<HomePage />}
-						/>
-					</Routes>
-				</Layout>
-			</div>
-		</BrowserRouter>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<BrowserRouter>
+				<div className="App">
+					<Layout>
+						<Routes>
+							<Route
+								path="/"
+								element={<HomePage />}
+							/>
+						</Routes>
+					</Layout>
+				</div>
+			</BrowserRouter>
+		</ThemeProvider>
 	);
 }
 
